0d3bd80da87ced0e129affc7cb5c3faa32a173e7
kmarie2001 <143460313+kmarie2001@users.noreply.github.com>
2023-09-22 15:19:06 -0600

    cleaned up code to display the usage of passing in parameters and returning values

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    static double radius;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner calculateCircle = new Scanner(System.in);
         System.out.println("Please enter the radius of your circle: ");
-        radius = Double.parseDouble(calculateCircle.nextLine());
-
-        calculateDiameter();
-        calculateArea();
-        calculateCircumference();
+        double radius = Double.parseDouble(calculateCircle.nextLine());
+        circleCalculations(2 * radius, Math.PI * (radius * radius), 2 * Math.PI * radius);
     }
 
-        public static void calculateDiameter(){
-
-            double diameter = 2 * radius;
-            System.out.println("The diameter of the circle is " + diameter + ".");
+        public static void circleCalculations(double diameter, double area, double circumference){
+        System.out.println("The diameter of the circle is " + diameter + ".");
+        System.out.println("The area of the circle is " + area + ".");
+        System.out.println("The circumference of the circle is " + circumference + ".");
         }
 
-        public static void calculateArea(){
-
-            double area = Math.PI * (radius * radius);
-            System.out.println("The area of the circle is " + area + ".");
         }
-
-        public static void calculateCircumference(){
-
-            double circumference = 2 * Math.PI * radius;
-            System.out.println("The circumference of the circle is " + circumference + ".");
-        }
-    }
